4917f0523a7ced9cccc60aa99446fd556fe4b61d
Rashed Momin <rashed347@gmail.com>
2018-03-06 06:54:10 +0600

    Add files via upload

---
+++ PracticeProject/PracticeProject/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProject
+{
+    class Employee
+    {
+
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+
+        // Properties!
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay) { }
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+        }
+        // Methods.
+        public void GiveBonus(float amount)
+        { Pay += amount; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+
+    }
+}
